8a9a79cb94ad610bbc91945d6a78fd7c4e1b3c95
Mikhail Spitsin <Mikhail.Spitsin@sidenis.com>
2014-07-21 10:18:48 +0400

    - added license text for all java classes

--- app/src/main/java/com/SpitsinStafichuk/vkazam_remastered/NavigationDrawerFragment.java
+++ app/src/main/java/com/SpitsinStafichuk/vkazam_remastered/NavigationDrawerFragment.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2014 SpitsinStafichuk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package com.SpitsinStafichuk.vkazam_remastered;
 
 
@@ -26,6 +41,9 @@ import android.widget.Toast;
  * Fragment used for managing interactions for and presentation of a navigation drawer.
  * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
  * design guidelines</a> for a complete explanation of the behaviors implemented here.
+ *
+ * @author Michael Spitsin
+ * @since 2014-07-21
  */
 public class NavigationDrawerFragment extends Fragment {
 
